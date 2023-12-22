bbfbdf7743e626139013d19cc8c474187d235a9b
Sadi Barkın Şeber <sadibarkn@gmail.com>
2023-12-22 13:44:43 +0300

    Add project files.

---
+++ HtmlandTagHelper/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using HtmlandTagHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HtmlandTagHelper.Controllers
+{
+    public class UserController : Controller
+    {
+
+    
+
+        public IActionResult HtmlHelpers()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult HtmlHelpers(Userr userr)
+        {
+            return View();
+        }
+
+        public IActionResult TagHelpers() => View();
+        [HttpPost]
+        public IActionResult TagHelpers(Userr userr)
+        {
+            Userr usserr = new Userr()
+            {
+                    UserName="testuser",
+                    Password="12345",
+                    FirstName="John",
+                    LastName="Doe"
+            };
+
+            // return View("HtmlHelpers",userr);
+            return RedirectToAction("HtmlHelpers", "User");
+
+        }
+
+
+    }
+
+   
+
+}
---
+++ HtmlandTagHelper/Models/Userr.cs
@@ -0,0 +1,11 @@
+﻿namespace HtmlandTagHelper.Models
+{
+    public class Userr
+    {
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
